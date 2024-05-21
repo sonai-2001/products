e719372592a044f547601ccd4a359ff0cc95b04e
sonai-2001 <sonaibhadra11174@gmail.com>
2024-05-21 13:15:06 +0530

    feat-routing2

--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -8,7 +8,7 @@ import Edit from '../components/Edit.jsx'
 function Routing() {
   return (
    <Routes>
-    <Route path='/products'  element={<Home/>}/>
+    <Route path='/products/'  element={<Home/>}/>
     <Route path='/products/add'  element={<Add/>}/>
     <Route path='/products/edit/:pid'  element={<Edit/>}/>
     <Route path='/products/product/:pid'  element={<Details/>}/>
